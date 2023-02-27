a4c01498162f5d95bec23d591495a83ae2768f11
konnor <konnor.frik666@gmail.com>
2023-02-27 23:48:33 +0500

    make a python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="eng",
+    version="0.1",
+    description="A weird game engine based on pygame",
+    author="Konnor Frik",
+    #author_email="jdoe@example.com",
+    packages=["eng"],
+    install_requires=[
+        "colors",
+    ],
+)
